71c45238035aba89c69fd0e34b9532c8e4f5cfef
peterandriakos <peter.andriakos@maine.edu>
2016-02-01 09:27:50 -0500

    Conflict Change

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
+		System.out.println("New Message... Deal with it");
 		System.out.println("Hello world!");
 		System.out.println("Hello world! (x2!!)");
 	}
