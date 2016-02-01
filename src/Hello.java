2cffff2868ec9e5de397d04e5b27c6d6704ff73a
peterandriakos <peter.andriakos@maine.edu>
2016-02-01 08:43:59 -0500

    HelloWorld2 w/ Preamble Message

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Here ye, here ye: important announcement.");
 		System.out.println("Hello world!");
 		System.out.println("Hello world! (x2!!)");
 	}
